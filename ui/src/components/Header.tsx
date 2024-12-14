2cad1a8e3a0c8f7413463f06051d9e4267fb0564
Mitko <mgeorgiev317@gmail.com>
2024-12-15 01:30:49 +0200

    Home page

---
+++ ui/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="container pt-5">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 lexend-bold">
+          Welcome, user123!
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default Header;
--- ui/src/pages/home-page/HomePage.tsx
+++ ui/src/pages/home-page/HomePage.tsx
@@ -1,9 +1,61 @@
 import "../../App.css";
 import "./HomePage.css";
 import logo from "../../assets/vault-logo-simplistic.svg";
+import addImg from "../../assets/add.svg";
+import scanImg from "../../assets/scan.svg";
+import folderImg from "../../assets/folder.svg";
+import Header from "../../components/Header";
 
 function HomePage() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <nav className="navbar">
+        <div className="container-fluid flex-column justify-content-center pt-5">
+          <img
+            src={logo}
+            alt="Vault Logo"
+            className="mb-4"
+            style={{ width: "5vh" }}
+          />
+          <h2 className="lexend-bold fs-1">My warranties</h2>
+        </div>
+      </nav>
+
+      <div className="container-fluid d-flex flex-column align-items-center text-center mt-5">
+        <div className="card add-card mb-4" style={{ width: "70vw" }}>
+          <img
+            src={addImg}
+            className="card-img-top mx-auto pt-3"
+            style={{ width: "5vh" }}
+          />
+          <div className="card-body">
+            <p className="card-text">Manually add a warranty</p>
+          </div>
+        </div>
+        <div className="card scan-card mb-4" style={{ width: "70vw" }}>
+          <img
+            src={scanImg}
+            className="card-img-top mx-auto pt-3"
+            style={{ width: "5vh" }}
+          />
+          <div className="card-body">
+            <p className="card-text">Scan a warranty</p>
+          </div>
+        </div>
+        <div className="card manage-card mb-4" style={{ width: "70vw" }}>
+          <img
+            src={folderImg}
+            className="card-img-top mx-auto pt-3"
+            style={{ width: "5vh" }}
+          />
+          <div className="card-body">
+            <p className="card-text">Manage warranties</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
