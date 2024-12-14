c3358e9b1e5f13a7c3055e39bffd9f8328c55e99
Mitko <mgeorgiev317@gmail.com>
2024-12-14 23:08:57 +0200

    Login page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/login-page/LoginPage";
+import HomePage from "./pages/home-page/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ ui/src/pages/home-page/HomePage.tsx
@@ -0,0 +1,9 @@
+import "../../App.css";
+import "./HomePage.css";
+import logo from "../../assets/vault-logo-simplistic.svg";
+
+function HomePage() {
+  return <></>;
+}
+
+export default HomePage;
