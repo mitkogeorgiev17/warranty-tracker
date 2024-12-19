77edca91a7a407de9cbe9e31a68053f75f603e07
Mitko <mgeorgiev317@gmail.com>
2024-12-19 23:45:11 +0200

    Deleting warranties functionality (FE)

--- ui/src/components/warranty-list/WarrantyList.tsx
+++ ui/src/components/warranty-list/WarrantyList.tsx
@@ -2,13 +2,50 @@ import editImg from "../../assets/edit-icon.svg";
 import deleteImg from "../../assets/delete-icon.svg";
 import "./WarrantyList.css";
 import { Warranty } from "../../types/Warranty";
+import axiosApi from "../../config/axiosApiConfig";
+import { API_BASE_URL, ENDPOINTS } from "../../config/apiConstants";
+import { toast } from "sonner";
+import { useState, useEffect } from "react";
 
 interface WarrantyListProps {
   items: Warranty[];
 }
 
 function WarrantyList(props: WarrantyListProps) {
-  const warrantyList = props.items.map((warranty) => (
+  const [warranties, setWarranties] = useState(props.items);
+
+  useEffect(() => {
+    setWarranties(props.items);
+  }, [props.items]);
+
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+
+  const handleDeleteClick = (id: number) => {
+    if (isDeleting) return;
+
+    setIsDeleting(true);
+    const endpoint = ENDPOINTS.DELETE_WARRANTY;
+
+    axiosApi({
+      method: endpoint.method,
+      url: `${API_BASE_URL}${endpoint.path}${id}`,
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          setWarranties(warranties.filter((w) => w.id !== id));
+          toast.success("Warranty deleted successfully.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting warranty:", error);
+        toast.error("Warranty could not be deleted.");
+      })
+      .finally(() => {
+        setIsDeleting(false);
+      });
+  };
+
+  const warrantyList = warranties.map((warranty) => (
     <li key={warranty.id}>
       <div className="card shadow-sm position-relative mb-3">
         <div className="d-flex justify-content-between mt-2 mx-2">
@@ -17,7 +54,10 @@ function WarrantyList(props: WarrantyListProps) {
             <button className="btn btn-sm btn-update">
               <img src={editImg} height="25px" />
             </button>
-            <button className="btn btn-sm btn-delete">
+            <button
+              className="btn btn-sm btn-delete"
+              onClick={() => handleDeleteClick(warranty.id)}
+            >
               <img src={deleteImg} height="20px" />
             </button>
           </div>
@@ -38,6 +78,17 @@ function WarrantyList(props: WarrantyListProps) {
   return (
     <>
       <div className="container my-3 text-normal">
+        {warrantyList.length === 0 && (
+          <div
+            className="container-fluid d-flex flex-column justify-content-center align-items-center"
+            style={{ height: "calc(100vh - 200px)" }}
+          >
+            <h3 className="text-thin opacity-50 text-center">
+              No warranties available. <br />
+              Please add a warranty to get started.
+            </h3>
+          </div>
+        )}
         <ul>{warrantyList}</ul>
       </div>
     </>
