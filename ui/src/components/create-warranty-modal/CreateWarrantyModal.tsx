36e73e0e03db1626b3fd1ab1154998fb3e052ec9
Mitko <mgeorgiev317@gmail.com>
2024-12-19 18:17:18 +0200

    Create warranty functionality

--- ui/src/components/create-warranty-modal/CreateWarrantyModal.tsx
+++ ui/src/components/create-warranty-modal/CreateWarrantyModal.tsx
@@ -1,29 +1,96 @@
+import { useState } from "react";
 import "./CreateWarrantyModal.css";
 import addIcon from "../../assets/add-icon.svg";
+import axiosApi from "../../config/axiosApiConfig";
+import { ENDPOINTS, API_BASE_URL } from "../../config/apiConstants";
+
+interface createWarrantyCommand {
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  notes: string;
+  category: string;
+}
+
+const defaultFormData = {
+  name: "",
+  startDate: "",
+  endDate: "",
+  notes: "",
+  category: "",
+};
+
+function createWarranty(createWarrantyCommand: createWarrantyCommand) {
+  const endpoint = ENDPOINTS.CREATE_WARRANTY;
+  axiosApi({
+    method: endpoint.method,
+    url: `${API_BASE_URL}${endpoint.path}`,
+    data: createWarrantyCommand,
+  })
+    .then((response) => console.log(response.data))
+    .catch((error) => console.error("Error creating warranty:", error));
+}
 
 function CreateWarrantyModal() {
+  const [formData, setFormData] = useState(defaultFormData);
+  const { name, startDate, endDate, notes, category } = formData;
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const warrantyCommand: createWarrantyCommand = {
+      ...formData,
+      startDate: new Date(formData.startDate),
+      endDate: new Date(formData.endDate),
+    };
+    createWarranty(warrantyCommand);
+    setFormData(defaultFormData);
+  };
+
   return (
     <>
       <div className="container d-flex flex-column justify-content-center align-items-center pt-4 warranty-form">
-        <div>
+        <form onSubmit={onSubmit}>
           <div className="mb-3">
             <label className="form-label text-bold">Name</label>
             <input
               type="text"
               className="form-control text-normal mb-3"
               id="name"
+              value={name}
+              onChange={onChange}
               placeholder="Name your warranty"
             />
           </div>
 
           <div className="mb-3">
             <label className="form-label text-bold">Start Date</label>
-            <input type="date" className="form-control" id="startDate" />
+            <input
+              type="date"
+              className="form-control"
+              id="startDate"
+              value={startDate}
+              onChange={onChange}
+            />
           </div>
 
           <div className="mb-3">
             <label className="form-label text-bold">End Date</label>
-            <input type="date" className="form-control" id="endDate" />
+            <input
+              type="date"
+              className="form-control"
+              id="endDate"
+              value={endDate}
+              onChange={onChange}
+            />
           </div>
 
           <div className="mb-3">
@@ -32,6 +99,8 @@ function CreateWarrantyModal() {
               className="form-control"
               id="notes"
               style={{ height: "10vh" }}
+              value={notes}
+              onChange={onChange}
             />
           </div>
 
@@ -39,16 +108,23 @@ function CreateWarrantyModal() {
             <label className="form-label text-bold">Category</label>
             <input
               type="text"
+              placeholder="Choose category (Optional)"
               className="form-control text-normal mb-3"
               id="category"
-              placeholder="Choose category (Optional)"
+              value={category}
+              onChange={onChange}
             />
           </div>
 
           <div className="d-flex justify-content-center pt-5">
-            <img src={addIcon} style={{ height: "20vh" }} />
+            <button
+              type="submit"
+              style={{ background: "none", border: "none" }}
+            >
+              <img src={addIcon} style={{ height: "20vh" }} alt="Add Icon" />
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
