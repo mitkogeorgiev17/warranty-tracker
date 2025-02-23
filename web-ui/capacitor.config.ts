00562fb432f86dca46e80076159cb091e36aea2a
Mitko <mgeorgiev317@gmail.com>
2025-02-23 20:54:30 +0200

    Capacitor + android project setup

---
+++ web-ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mitko.warranty.vault',
+  appName: 'warranty-vault-ui',
+  webDir: 'dist'
+};
+
+export default config;
